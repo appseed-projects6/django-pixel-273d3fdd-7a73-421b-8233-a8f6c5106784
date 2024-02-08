e97673ff048949297c93aabbd9bb1ca23dad46bc
AppSeed Generated Projects <133564452+appseed-projects6@users.noreply.github.com>
2024-02-08 15:16:01 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,58 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    user_name = models.CharField(max_length=255, null=True, blank=True)
+    password = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Custom User(models.Model):
+
+    #__Custom User_FIELDS__
+    user_name = models.CharField(max_length=255, null=True, blank=True)
+    password = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Custom User_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Custom User")
+        verbose_name_plural = _("Custom User")
+
+
+class Transfer Info(models.Model):
+
+    #__Transfer Info_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    school = models.CharField(max_length=255, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Transfer Info_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Transfer Info")
+        verbose_name_plural = _("Transfer Info")
+
+
+
+#__MODELS__END
